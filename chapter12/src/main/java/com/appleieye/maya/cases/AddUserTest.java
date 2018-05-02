78bf32811bcb52997c29a581b320fcb0399204f5
clarence <clarence@clarencedeMacBook-Pro.local>
2018-05-03 07:42:22 +0800

    commit

--- chapter12/src/main/java/com/appleieye/maya/cases/AddUserTest.java
+++ chapter12/src/main/java/com/appleieye/maya/cases/AddUserTest.java
@@ -13,6 +13,7 @@ import org.testng.annotations.Test;
 import sun.security.timestamp.TSRequest;
 
 import java.io.IOException;
+import java.sql.Time;
 
 import static org.testng.Assert.assertEquals;
 
@@ -25,13 +26,14 @@ import static org.testng.Assert.assertEquals;
 public class AddUserTest {
 
     @Test(dependsOnGroups = "loginTrue")
-    public void addUser() throws IOException {
+    public void addUser() throws IOException, InterruptedException {
         SqlSession sqlSession = DatabaseUtils.getSession();
         AddUserCase addUserCase = sqlSession.selectOne("addUserCase",1);
         System.out.println(addUserCase);
         System.out.println(TestConfig.addUserUrl);
 
         String result = getResult(addUserCase);
+        Thread.sleep(5000);
         assertEquals(addUserCase.getExpected(),result);
 
 
@@ -41,12 +43,12 @@ public class AddUserTest {
     public String getResult(AddUserCase addUserCase) throws IOException {
         HttpPost post = new HttpPost(TestConfig.addUserUrl);
         JSONObject param = new JSONObject();
-        param.put("username","haha");
-        param.put("password","1234");
-        param.put("age","19");
-        param.put("sex","男");
-        param.put("permission","1");
-        param.put("isDelete","1");
+        param.put("username",addUserCase.getUsername());
+        param.put("password",addUserCase.getPassword());
+        param.put("age",addUserCase.getAge());
+        param.put("sex",addUserCase.getSex());
+        param.put("permission",addUserCase.getPermission());
+        param.put("isDelete",addUserCase.getIsDelete());
 
         StringEntity entity = new StringEntity(param.toString(),"utf-8");
         post.setEntity(entity);
